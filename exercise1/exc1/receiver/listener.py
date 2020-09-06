d4b4b5921317b27313dbe7437e90f38cbba17d5a
Heng-G <70778934+Heng-G@users.noreply.github.com>
2020-09-06 19:53:09 +0200

    Update listener.py

--- exercise1/exc1/receiver/listener.py
+++ exercise1/exc1/receiver/listener.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 import rospy 
-from std_msgs.msg import Float32, Int16
+from std_msgs.msg import UInt16, Float32
 
 q =0.15
 
@@ -9,23 +9,25 @@ def callback(num):
     
     result = num.data/q
     print result
+    publish(result)
+    
+
+
+def publish(result):
     pub.publish(result)
-
-
-def listen():  
-    rospy.Subscriber('GUO', Int16, callback)
-
-  
+    rate.sleep()
 
 
 if __name__ == "__main__":
     rospy.init_node('nodeB')
     pub = rospy.Publisher('/kthfs/result', Float32, queue_size=10)
     rate = rospy.Rate(20) # 20hz
+    rospy.Subscriber('GUO', UInt16, callback)
 
-    listen()
+
 
     rospy.spin()
     
+
 
 
